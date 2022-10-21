924f4809a9a6a813f1b938a28a0161d5488e194a
Santiago Maza <santiagommaza@gmail.com>
2022-10-21 11:14:37 -0300

    Se agrego la estructura main a admin.html y se intentó corregir el boton editar, todavia sin exito

--- js/admin.js
+++ js/admin.js
@@ -69,3 +69,27 @@ const editarPeli = async (id) => {
     },
   })
 }
+
+
+
+
+
+
+
+
+
+(() => {
+  'use strict'
+
+  const forms = document.querySelectorAll('.needs-validation')
+
+  Array.from(forms).forEach(form => {
+    form.addEventListener('submit', event => {
+      if (!form.checkValidity()) {
+        event.preventDefault()
+        event.stopPropagation()
+      }
+      form.classList.add('was-validated')
+    }, false)
+  })
+})()
