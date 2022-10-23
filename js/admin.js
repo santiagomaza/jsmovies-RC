fbbe7238be5f0a6505854f44ea2854df7e87448d
nicolas <nico.fcok@gmail.com>
2022-10-22 21:33:07 -0300

    fixeando edicion

--- js/admin.js
+++ js/admin.js
@@ -3,6 +3,8 @@ const agregarPeli = () => {
   const categoria = document.getElementById("categoria").value;
   const descripcion = document.getElementById("descripcion").value;
 
+  console.log("Ejecutando AgregarPeli");
+
   if (titulo == "" || categoria == "" || descripcion == "") {
     return false;
   }
@@ -30,69 +32,21 @@ const tablaPelis = async () => {
   const tabla = await obtenerPelis();
   const div = document.getElementById("tablaPelis");
 
+  console.log("Agregando pelicula");
+
   const peliculas = tabla.map(
     (pelicula) => `
-  <table class="table table-bordered border-dark">
-  <tbody>
     <tr class="">
       <th scope="row">${pelicula.id}</th>
       <td>${pelicula.titulo}</td>
       <td>${pelicula.categoria}</td>
       <td>${pelicula.descripcion}</td>
-      <td><button type="submit" class="btn btn-danger" onclick="eliminarPeli(${pelicula.id})">
+      <td><button type="button" class="btn btn-danger" onclick="eliminarPeli(${pelicula.id})">
        <i class="bi bi-trash2-fill fs-5 rounded text-white"></i>
       </button>
-      <button type="button" class="btn btn-primary" onclick="" data-bs-toggle="modal" data-bs-target="#exampleModal2"><i class="bi bi-pencil-fill rounded text-white fs-5"></i></button>
+      <button type="button" class="btn btn-primary" onclick="editarPeli(${pelicula.id})" data-bs-toggle="modal" data-bs-target="#exampleModal2"><i class="bi bi-pencil-fill rounded text-white fs-5"></i></button>
       </td>
     </tr>
-  </tbody>
-</table>
- <div class="modal fade" id="exampleModal2" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                <div class="modal-dialog">
-                  <div class="modal-content">
-                    <div class="modal-header">
-                      <h1 class="modal-title fs-5" id="exampleModalLabel">Editar Pelicula</h1>
-                      <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                    </div>
-                    <div class="modal-body">
-                      <form class="row g-3 needs-validation" novalidate>
-                        <div class="col-md-12">
-                          <label for="validationCustom03" class="form-label">Nombre/Título de la película</label>
-                          <input type="text" class="form-control" id="nvTitulo" required>
-                          <div class="invalid-feedback">
-                            Ingrese el nombre de la película
-                          </div>
-                        </div>
-                        <div class="col-md-12">
-                          <label for="validationCustom04" class="form-label">Categoría</label>
-                          <select class="form-select" id="nvCategoria" required>
-                            <option selected disabled value="">Elegir categoría</option>
-                            <option>Accion</option>
-                            <option>Romantica</option>
-                            <option>Drama</option>
-                          </select>
-                          <div class="invalid-feedback">
-                            Ingrese la categoría de la película
-                          </div>
-                        </div>
-                        <div class="col-md-12">
-                          <label for="exampleFormControlTextarea1" class="form-label">Descripción</label>
-                          <textarea class="form-control" id="nvDescripcion" rows="3" required></textarea>
-                          <div class="invalid-feedback">
-                            Por favor inserte una descripción de la película 
-                          </div>
-                        </div>
-                        <div class="col-12">
-                        </div>
-                        <div class="modal-footer">
-                          <button type="reset" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
-                          <button type="submit" class="btn btn-primary" onclick="editarPeli(${pelicula.id})">Editar Pelicula</button>
-                        </div>
-                      </form>
-                    </div>
-                  </div>
-                </div>
-               </div>
     `
   );
 
@@ -108,26 +62,29 @@ const eliminarPeli = async (id) => {
 };
 
 const editarPeli = async (id) => {
-  const titulo = document.getElementById("nvTitulo").value;
-  const categoria = document.getElementById("nvCategoria").value;
-  const descripcion = document.getElementById("nvDescripcion").value;
-
   console.log("EJECUTANDO EDICION");
 
-  if (titulo == "" || categoria == "" || descripcion == "") {
-    return false;
-  }
+  let boton = document.getElementById("boton-cambiar");
+  boton.addEventListener("click", async () => {
+    const titulo = document.getElementById("nvTitulo").value;
+    const categoria = document.getElementById("nvCategoria").value;
+    const descripcion = document.getElementById("nvDescripcion").value;
+    console.log(titulo, categoria, descripcion, id);
 
-  await fetch(`http://localhost:3000/movies/${id}`, {
-    method: "PUT",
-    body: JSON.stringify({
-      titulo,
-      categoria,
-      descripcion,
-    }),
-    headers: {
-      "Content-type": "application/json; charset=UTF-8",
-    },
+    if (titulo == "" || categoria == "" || descripcion == "") {
+      return false;
+    }
+    await fetch(`http://localhost:3000/movies/${id}`, {
+      method: "PUT",
+      body: JSON.stringify({
+        titulo,
+        categoria,
+        descripcion,
+      }),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+    });
   });
 };
 
