902c8cb44ed8bf553fd568587d190034e2c881f3
unknown <nico.fcok@gmail.com>
2022-10-24 21:08:51 -0300

    agregando funcionalidad a las categorias para el mapeo de cards

---
+++ js/infantiles.js
@@ -0,0 +1,24 @@
+const mapeoPeliculas = async () => {
+  const peli = await obtenerPelis();
+  const div = document.getElementById("container_peliculas");
+  console.log("Agregando pelicula");
+
+  const peliculas = peli.filter(a => a.categoria == 'Infantiles')
+
+  const peliculas2 = peliculas.map(a => `
+  <div class="card" style="width: 15rem;">
+      <img src="${a.img}" class="card-img-top col-lg-3" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">${a.titulo}</h5>
+        <p class="card-text">Año: ${a.anio}</p>
+        <p class="card-text">Duracion: ${a.duracion}</p>
+        <p clas"card-text">Genero: ${a.categoria} </p>
+        <p class="card-text">${a.descripcion}</p>
+      </div>
+    </div>
+  `)
+ 
+  div.innerHTML = peliculas2.join("");
+}
+
+mapeoPeliculas()
